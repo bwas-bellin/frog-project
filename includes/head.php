ddea7613a109fdeeaecd29539484c89ca1f9f333
Charlie Schams <charlie.schams@gundersenhealth.org>
2023-11-13 21:05:34 +0000

    Add CSS stuff

--- includes/head.php
+++ includes/head.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head>  
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Frogs</title>
+    <link rel="stylesteet" href="bootstrap-3.4.1-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
-    <div class="main">
-        <nav>
+    <div class="container fluid">
+<nav class="navbar navbar-default">
             <ul>
                 <li>
                     <a href="index.php">Home</a>
@@ -20,19 +22,7 @@
                     <a href="diet.php">Diet</a>
                 </li>
                 <li>
-                    <a href="mating.php">Mating/Reproduction</a>
-                </li>
-                <li>
-                    <a href="anatomy.php">Anatomy and Biology</a>
-                </li>
-                <li>
                     <a href="behavior.php">Froggy Behavior</a>
                 </li>
-                <li>
-                    <a href="communication.php">Communication</a>
-                </li>
-                <li>
-                    <a href="habitat.php">Habitat</a>
-                </li>
             </ul>
-        </nav>
+</nav>
