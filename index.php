938bd3265d62ba3ef184d0df98f3a2074f23945f
bwas-bellin <benjamin.wasilewski@bellin.org>
2023-11-13 13:03:42 -0600

    add scientific classification to homepage

--- index.php
+++ index.php
@@ -2,5 +2,27 @@
 <div class="content">
     <h1>Frogs!</h1>
     <img src="img/frog.jpg" style="width: 600px; margin: 0 auto" /> 
+    <h1>Frog Facts: What's in a Name?</h1>
+    <p>Hey there, young scientists! Let's learn some cool facts about frogs, especially their scientific names and classification. 🐸</p>
+    
+    <h2>What's in a Name?</h2>
+    <p>The scientific name of a frog is <em>Anura</em>. This special name helps scientists talk about frogs without getting confused.</p>
+    
+    <h2>Frog Classification</h2>
+    <ul>
+        <li><strong>Kingdom:</strong> Animalia</li>
+        <li><strong>Phylum:</strong> Chordata</li>
+        <li><strong>Class:</strong> Amphibia</li>
+        <li><strong>Order:</strong> Anura</li>
+    </ul>
+    
+    <h2>Frog Families</h2>
+    <p>There are over 5,000 species of frogs, including true frogs, tree frogs, and toads.</p>
+    
+    <h2>Fun Fact</h2>
+    <p>The word <em>Anura</em> means 'without a tail' in Greek. Adult frogs don't have tails, but tadpoles do!</p>
+    
+    <h2>Fun Activity</h2>
+    <p>Draw your favorite frog and label it with its scientific name. Show what you've learned about frog classification!</p>
 </div>
 <?php include 'includes/foot.php'; ?>
